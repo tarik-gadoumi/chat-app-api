e03a2eb10b38a59100ab8ab97c098ed5786c4c55
tarik-gadoumi <tarik.gadoumi@smart-tribune.com>
2023-08-01 20:57:52 +0200

    -a

--- build/models/backListedTokenSchema.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const mongoose = require("mongoose");
-const blacklistedTokenSchema = new mongoose.Schema({
-    token: String,
-    createdAt: { type: Date, default: Date.now }
-});
-exports.BlacklistedToken = mongoose.model("BlacklistedToken", blacklistedTokenSchema);
-//# sourceMappingURL=backListedTokenSchema.js.map
--- build/resolvers/backListedTokenSchema.js
+++
@@ -1,1 +0,0 @@
-//# sourceMappingURL=backListedTokenSchema.js.map
